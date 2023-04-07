f84409dd51f6fe6c626b22587c203104ca1a1e8a
sriram <v.sriram.sscet@gmail.com>
2023-04-07 07:11:24 -0400

    added travis YML

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import React, { Component }  from 'react';
 
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
